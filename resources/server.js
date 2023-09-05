74b321b906b02e3e5857c015246792a6b2491193
Mikael Wallin <mikael.x.wallin@gmail.com>
2023-09-05 14:41:56 +0200

    Added server.js

--- resources/server.js
+++ resources/server.js
@@ -1,18 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-
+const express = require('express');
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(helmet());
-
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send({status:"success"});
+   res.json({status: "ok"});
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
